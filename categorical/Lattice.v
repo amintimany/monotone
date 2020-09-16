91d29dd282c22862942a55925d8ab0f929b242b5
Amin Timany <amintimany@gmail.com>
2020-09-16 16:13:54 +0200

    Inclusion from JSLB_cat to PCM_cat

--- categorical/Lattice.v
+++ categorical/Lattice.v
@@ -51,6 +51,17 @@ Proof.
   - apply join_UB1.
 Qed.
 
+Lemma join_order (j : JSLB) (a b : j) : j a b ↔ join j a b = b.
+Proof.
+  split.
+  - intros Hab.
+    apply PO_antisymm.
+    + apply join_LUB; [assumption|reflexivity].
+    + apply join_UB2.
+  - intros <-.
+    apply join_UB1.
+Qed.
+
 Record JSLB_morphism (j j' : JSLB) := {
   JSLBM_mor :> PO_morphism j j';
   JSLBM_resp x y : JSLBM_mor (join j x y) = join j' (JSLBM_mor x) (JSLBM_mor y);
@@ -109,7 +120,7 @@ Program Definition JSLB_forgetful_functor : Functor JSLB_cat PO_cat :=
   {| FO := JSLB_PO;
      FA := @JSLBM_mor; |}.
 
-Program Definition RA_of_JSLB (j : JSLB) : PCM :=
+Program Definition PCM_of_JSLB (j : JSLB) : PCM :=
 {| PCM_car := PO_type j;
    op := join j;
    unit := bot j;
@@ -128,3 +139,83 @@ Proof.
   rewrite join_comm. apply join_bot.
 Qed.
 
+Local Obligation Tactic := idtac.
+
+Program Definition PCM_of_JSLB_morph (j j' : JSLB) (f : JSLB_morphism j j') :
+  PCM_morphism (PCM_of_JSLB j) (PCM_of_JSLB j') :=
+{| PCMM_mor := f |}.
+
+Next Obligation.
+Proof.
+  simpl; trivial.
+Qed.
+Next Obligation.
+Proof.
+  intros j j' f x Hx; simpl in *.
+  rewrite JSLBM_resp; trivial.
+Qed.
+Next Obligation.
+Proof.
+  intros j j' f; simpl.
+  rewrite JSLBM_bot; trivial.
+Qed.
+
+Program Definition PCM_of_JSLB_morph_back_PO_morphism
+        (j j' : JSLB) (f : PCM_morphism (PCM_of_JSLB j) (PCM_of_JSLB j')) :
+  PO_morphism j j' :=
+{| POM_mor := f |}.
+
+Next Obligation.
+Proof.
+  intros j j' f x y Hxy.
+  apply join_order.
+  pose proof (PCMM_resp _ _ f) as Hresp; simpl in Hresp.
+  rewrite <- Hresp.
+  apply join_order in Hxy as ->; trivial.
+Qed.
+
+Program Definition PCM_of_JSLB_morph_back
+        (j j' : JSLB) (f : PCM_morphism (PCM_of_JSLB j) (PCM_of_JSLB j')) :
+  JSLB_morphism j j' :=
+{| JSLBM_mor := PCM_of_JSLB_morph_back_PO_morphism _ _ f |}.
+
+Next Obligation.
+Proof.
+  intros j j' f x y; simpl in *.
+  rewrite PCMM_resp; trivial.
+Qed.
+Next Obligation.
+Proof.
+  intros j j' f; simpl in *.
+  rewrite PCMM_unit; trivial.
+Qed.
+
+Program Definition PCM_of_JSLB_func : Functor JSLB_cat PCM_cat :=
+{| FO := PCM_of_JSLB; FA := PCM_of_JSLB_morph |}.
+
+Next Obligation.
+Proof.
+  intros j.
+  apply PCMM_morphism_eq; trivial.
+Qed.
+Next Obligation.
+Proof.
+  intros j j' j'' f g; simpl in *.
+  apply PCMM_morphism_eq; trivial.
+Qed.
+
+Lemma PCM_of_JSLB_func_full : Full_Func PCM_of_JSLB_func.
+Proof.
+  intros j j' f; simpl in *.
+  exists (PCM_of_JSLB_morph_back _ _ f).
+  apply PCMM_morphism_eq; trivial.
+Qed.
+
+Lemma PCM_of_JSLB_func_faithful : Faithful_Func PCM_of_JSLB_func.
+Proof.
+  intros j j' f g Hfg; simpl in *.
+  apply JSLBM_morphism_eq.
+  apply POM_morphism_eq.
+  intros x.
+  apply (equal_f (f_equal PCMM_mor Hfg) x).
+Qed.
